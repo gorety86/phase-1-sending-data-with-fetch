25d23c905661a28831f3713a5a33e38d4f32e676
gorety <wagutugorety@gmail.com>
2023-04-01 15:42:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const submitData = {
+  Name: "Doreen",
+  Email: "doreen@gmail.com",
+};
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({submitData}),
+};
+function submitData() {
+  fetch(`http://localhost:3000/users`, configurationObject)
+  .then(function (response) {
+    return response.json();})
+  .then(function (object) {
+    console.log(object);
+  })
+    .catch(function(error) {
+      alert("message");
+      console.log(error.message);
+    });
+  
+};
+
+
+  
